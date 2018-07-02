d5a332b715b9f64053256a94eefafc4ae8954c25
Utwo <mihai.legat@gmail.com>
2018-07-02 14:16:54 +0300

    Add travis. Delete dist folder.

--- dist/transformers/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var styles_1 = require("./styles");
-var transformers = {
-    styles: styles_1.default,
-};
-exports.default = transformers;
-//# sourceMappingURL=index.js.map
