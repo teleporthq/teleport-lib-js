360d06e2a657d08abac9ec7501fcbd3ba5448910
Paul <aelythe@gmail.com>
2018-07-05 14:49:46 +0300

    expose Target class too

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Teleport from './Teleport'
 
 export default Teleport
+export { default as Target } from './lib/Target'
 export { default as Generator } from './lib/Generator'
 export { default as Publisher } from './lib/Publisher'
 export { default as ComponentGenerator } from './lib/Generator/Component'
