a5b6c3a3f1fb2c0ad0d15ae55a5a58a7ce95bd65
Paul <aelythe@gmail.com>
2018-07-09 13:59:59 +0300

    export ElementsLibrary

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import Teleport from './Teleport'
-import * as Types from './types'
+import * as Types from './index'
 
 export default Teleport
 export { default as Target } from './lib/Target'
+export { default as ElementsLibrary } from './lib/ElementsLibrary'
 export { default as Generator } from './lib/Generator'
 export { default as Publisher } from './lib/Publisher'
 export { default as ComponentGenerator } from './lib/Generator/Component'
