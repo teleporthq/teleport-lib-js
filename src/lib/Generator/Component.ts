ca1487d653fcf56142b019633c2fd4894a35be08
Cristian Moldovan <crissmoldovan@gmail.com>
2018-08-26 04:03:02 +0200

    removed generate method, as signature differs by implementation

--- src/lib/Generator/Component.ts
+++ src/lib/Generator/Component.ts
@@ -1,16 +1,9 @@
 import Generator from '../Generator'
-import FileSet from './FileSet'
-import { Component } from '../../types'
-import { Options } from 'prettier'
 
 export default class ComponentGenerator {
   public generator: Generator
 
   constructor(generator: Generator) {
     this.generator = generator
-  }
-
-  public generate(component: Component, options?: { prettier: Options }): FileSet {
-    throw new Error("COMPONENT GENERATOR'S GENERATE METHOD SHOULD NOT BE INVOKED DIRECTLY! Please use a target specific generator")
   }
 }
