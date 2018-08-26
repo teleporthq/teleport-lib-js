2d3be153318c58497c4b581f8f137479b622f8d0
Cristian Moldovan <crissmoldovan@gmail.com>
2018-08-26 04:04:05 +0200

    removed generate method, as signature differs by implementation

--- src/lib/Generator/Project.ts
+++ src/lib/Generator/Project.ts
@@ -1,16 +1,9 @@
-import { Project } from '../../types'
 import Generator from '../Generator'
-import FileSet from './FileSet'
-import { Options } from 'prettier'
 
 export default class ProjectGenerator {
   public generator: Generator
 
   constructor(generator: Generator) {
     this.generator = generator
-  }
-
-  public generate(project: Project, options?: { prettier: Options }): FileSet {
-    throw new Error("PROJECT GENERATOR'S GENERATE METHOD SHOULD NOT BE INVOKED DIRECTLY! Please use a target specific generator")
   }
 }
