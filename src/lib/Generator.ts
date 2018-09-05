ed52b999d6107fc354756ce8eb94a1a8afcb26a3
Cristian Moldovan <crissmoldovan@gmail.com>
2018-09-05 08:18:36 +0300

    added renderer support to generator

--- src/lib/Generator.ts
+++ src/lib/Generator.ts
@@ -1,6 +1,8 @@
 import Target from './Target'
 import ComponentGenerator from './Generator/Component'
 import ProjectGenerator from './Generator/Project'
+import Renderer from './Generator/Renderer'
+import FileSet from './Generator/FileSet'
 
 export default class Generator {
   public name: string
@@ -10,12 +12,23 @@ export default class Generator {
   public componentGenerator: ComponentGenerator
   public projectGenerator: ProjectGenerator
 
-  constructor(name: string, targetName: string) {
+  constructor(name: string, targetName: string, componentRenderers?: { [key: string]: Renderer }) {
     this.name = name
     this.targetName = targetName
+
+    this.componentGenerator = new ComponentGenerator(this, componentRenderers)
+    this.projectGenerator = new ProjectGenerator(this, this.componentGenerator)
   }
 
   public setTarget(target: Target): void {
     this.target = target
+  }
+
+  public generateComponent<T, U>(component: T, options: U): FileSet {
+    return this.componentGenerator.generate(component, options)
+  }
+
+  public generateProject(component: any, options: any): FileSet {
+    return this.projectGenerator.generate(component, options)
   }
 }
